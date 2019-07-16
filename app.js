0899274dbb94b7287518ede86007eaef0f75d054
Liam Dawson <liamrdawson@gmail.com>
2019-07-16 22:59:46 +0100

    add index.pug initial template

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set(`view engine`, `pug`);
+
 app.get('/', (req, res) => {
-    res.send('<h1>It is alivvvee!!</h1>');
+    res.render('index');
 });
 
 app.get('/hello', (req, res) => {
