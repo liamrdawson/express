42d82435e50886ac466e2fcc620ca5f8b9f5940a
Liam Dawson <liamrdawson@gmail.com>
2019-07-16 22:26:55 +0100

    add get methods and port listener

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+app.get('/', (req, res) => {
+    res.send('<h1>It is alivvvee!!</h1>');
+});
+
+app.get('/hello', (req, res) => {
+    res.send(`<h1>Hello, is it me you're coding for!?</h1>`);
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000')
+});
