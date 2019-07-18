81b22d3eb99b8b9f7ce266eabc8ad4a750cea1bf
Liam Dawson <liamrdawson@gmail.com>
2019-07-18 22:18:55 +0100

    set up cookies to store username data, and redirect for goodbye button

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
 
-  
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false}));
+
+
 app.set(`view engine`, `pug`);
 
+
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', { name });
+    } else {
+        res.redirect(`/hello`);
+    }
+    
 });
 
 app.get('/cards', (req, res) => {
@@ -15,12 +26,25 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-    res.render(`hello`);
+    const name = req.cookies.username;
+    if (name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
 });
 
+
 app.post('/hello', (req, res) => {
-    res.render(`hello`, { name: req.body.username });
+    res.cookie(`username`, req.body.username);
+    res.redirect(`/`);
 });
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
+});
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000')
