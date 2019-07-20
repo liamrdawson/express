dad3e5887b81f7ca379aa808d5dd82b915097e31
Liam Dawson <liamrdawson@gmail.com>
2019-07-20 22:53:58 +0100

    add logic to randomise card being shown at /cards

--- routes/cards.js
+++ routes/cards.js
@@ -3,16 +3,27 @@ const router    = express.Router();
 const { data }  =  require(`../data/flashcardData.json`);
 const { cards } = data;
 
+router.get(`/`, (req, res) => {
+    const numberOfCards = cards.length;
+    const flashCardId = Math.floor(Math.random() * numberOfCards);
+    res.redirect(`/cards/${flashCardId}?side=question`);
+});
+
 router.get('/:id', (req, res) => {
     const {side} = req.query;
     const {id} = req.params;
     const text = cards[id][side];
     const {hint} = cards[id];
 
-    const templateData = { text };
+    const templateData = { id, text };
 
     if (side === `question`) {
         templateData.hint = hint;
+        templateData.sideToShow = `answer`;
+        templateData.sideToShowDisplay = `Answer`;
+    } else if (side === `answer`) {
+        templateData.sideToShow = `question`;
+        templateData.sideToShowDisplay = `Question`;
     }
 
     res.render('card', templateData);
